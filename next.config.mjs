aa667f55bd97c78c410cbf316f054190522e68b8
juanmig7890 <juanmig2708m@gmail.com>
2026-05-19 09:53:19 -0500

    configurando render

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'image.tmdb.org' },
       { protocol: 'http', hostname: 'localhost' },
